b4d4598a6b787508f090d5016fcbc965602c8317
priyanshukmr <87386297+priyanshukmr@users.noreply.github.com>
2023-08-24 01:29:00 +0530

    Update Merge SORT.cpp

--- Merge SORT.cpp
+++ Merge SORT.cpp
@@ -14,3 +14,30 @@
         for(int i=ss; i<=se; i++) 
             nums[i]=sorted[i-ss];
     }
+
+    ll inv_count(int arr[], int start, int end) {
+        if(start>=end) return 0;
+        int mid = (start+end)>>1;
+        ll left_cnt = inv_count(arr, start, mid);
+        ll right_cnt = inv_count(arr, mid+1, end);
+        int leftIdx=start, rightIdx=mid+1;
+        ll cur_cnt = 0;
+        vector<int> sorted;
+        
+        while(leftIdx<=mid || rightIdx<=end) {
+            if(leftIdx>mid) {
+                sorted.push_back(arr[rightIdx++]);
+            }
+            else if(rightIdx>end || arr[leftIdx] <= arr[rightIdx]) {
+                sorted.push_back(arr[leftIdx++]);
+            }
+            else {
+                cur_cnt += mid-leftIdx+1;
+                sorted.push_back(arr[rightIdx++]);
+            }
+        }
+        for(int i=start; i<=end; i++) {
+            arr[i] = sorted[i-start];
+        }
+        return left_cnt+cur_cnt+right_cnt;
+    }
