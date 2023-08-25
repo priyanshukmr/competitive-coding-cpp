64f1bbe4e5559b3a561d7a9018e393530080f52b
priyanshukmr <87386297+priyanshukmr@users.noreply.github.com>
2023-08-26 02:16:47 +0530

    Update topo_sort_DAG_KAHNS_ALGO.cpp

--- Graph/topo_sort_DAG_KAHNS_ALGO.cpp
+++ Graph/topo_sort_DAG_KAHNS_ALGO.cpp
@@ -37,65 +37,28 @@ bool isCyclic() {
 
 //BFS-based
 
-/****   KAHN's ALGORITHM toposort using BFS.     ****/
+/*      KAHN's ALGORITHM toposort using BFS.     */
 // Problem: lexicographically smallest topological sort https://www.spoj.com/problems/TOPOSORT/
 // detects cycle also (no toposort exists)
 
-#include<bits/stdc++.h>
-using namespace std;
-#define ll long long int
-#define vec vector
-#define pb push_back
-#define s(x) scanf("%d",&x)
-#define all(x)	x.begin(),x.end()
-#define pii pair<int,int>
-#define fi first
-#define se second
-#define SZ 100005
-#define boost ios::sync_with_stdio(0);
-
-vec<int> adj[100005];
-int in[100005]; //stores indegree of each vertex
-vec<int> topo;
-int vis_cnt = 0;
-priority_queue <int, vector<int>, greater<int>> q; //min-heap
-
-void bfs_topo()
-{
-	while (!q.empty()) {
-		int u=q.top();
-		q.pop();
-		topo.pb(u);
-        vis_cnt++;
-		
-		for(int v:adj[u]) {
-			in[v]--;
-			if(in[v]==0)
-			q.push(v);
-		} 
-	}
-}
-
-int main()
-{
-	int n,m,u,v;
-    cin >> n >> m;
-
-    for(int i=0; i<m; i++){
-        cin >> u >> v;
-        adj[u].pb(v);
-		in[v]++;
-	}
-
-    for (int i = 1; i <= n; i++){
-        if(in[i] == 0)
-            q.push(i);
+vector<int> kahns_algo(vector<int> &indegree) {
+    vector<int> topo;
+    set<int> minHeap;
+    for(int i=1; i<=n; i++) {
+        if(!indegree[i]){
+            minHeap.insert(i);
+        }
     }
-    bfs_topo();
-	if(vis_cnt < n) {
-        cout << "Sandro fails.";
-        return 0;
-    }  
-	for(int i=0; i<topo.size(); i++)
-        cout << topo[i] << " ";
+    while(!minHeap.empty()) {
+        int smallest = *minHeap.begin();
+        topo.push_back(smallest);
+        minHeap.erase(smallest);
+        for(int v: adj[smallest]) {
+            indegree[v]--;
+            if(indegree[v] == 0) {
+                minHeap.insert(v);
+            }
+        }
+    }
+    return topo;
 }
