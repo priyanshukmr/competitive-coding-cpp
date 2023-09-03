93b36d463c6060397c55434293f69600a3949cf9
priyanshukmr <87386297+priyanshukmr@users.noreply.github.com>
2023-09-03 18:30:33 +0530

    Update Kosaraju algo_SCC.cpp

--- Graph/Kosaraju algo_SCC.cpp
+++ Graph/Kosaraju algo_SCC.cpp
@@ -1,103 +1,34 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-#define lli long long int
-lli v;
-
-void DFS(vector<lli> graph[],lli source,lli v,lli visited[])
-{
-  stack<lli> Q;
-  Q.push(source);
-  visited[source]=1;
-
-  while(!Q.empty())
-  {
-      lli vertex=Q.top();
-      cout<<vertex;
-      Q.pop();
-      for(lli i=0;i<graph[vertex].size();i++)
-      {
-          if(!visited[ graph[vertex][i] ])
-          {
-              Q.push(graph[vertex][i]);
-              visited[ graph[vertex][i] ]=1;
-          }
-      }
-  }
-
-}
-
-//graph1 is original,graph2 is reversed!
-void reverse_this(vector<lli> graph[],vector<lli> graph2[],lli v)
-{
-    for(lli i=1;i<=v;i++)
-    {
-        for(lli j=0;j<graph[i].size();j++)
-        graph2[ graph[i][j] ].push_back(i);
-    }
-
-}
-
-//makes the finish time stack(decreasing),'not reverse of dfs' ,,note dat we HAVE to use recursive dfs
-void make_stack(vector<lli> graph[],stack<lli> &finish,lli visited[],lli source,lli v)
-{
-    visited[source]=1;
-
-    for(lli i=0; i<graph[source].size() ;i++)
-    {
-        if(!visited[ graph[source][i] ])
-        make_stack(graph, finish,visited,graph[source][i],v);
-    }
+//Problem: https://cses.fi/problemset/task/1682/
 
 
+int n,m;
+vector<int> adj[SZ];
+vector<int> rev_adj[SZ];
+vector<int> finished;
+vector<bool> vis(SZ, false);
 
-    finish.push(source);
-}
-
-void get_SCC(vector<lli> graph[],lli v)
-{
-    lli visited[v+1];
-    for(lli i=1;i<=v;i++)
-        visited[i]=0;
-
-    stack<lli> finish;
-
-    for(lli i=1;i<=v;i++)
-    if(!visited[i])
-    make_stack(graph,finish,visited,i,v);
-
-    for(lli i=1;i<=v;i++) //for dfs on reversed graph
-    visited[i]=0;
-
-    vector<lli> graph2[v+1];
-    reverse_this(graph,graph2,v);
-
-
-    while(!finish.empty())
-    {
-        lli popped=finish.top();
-        finish.pop();
-
-        if(!visited[popped])
-        {
-            DFS(graph2,popped,v,visited);
-            cout<<endl;
+void dfs(int u, vector<int> adj[], bool createFinish) {
+    vis[u] = true;
+    for(int v: adj[u]) {
+        if(!vis[v]) {
+            dfs(v, adj, createFinish);
         }
     }
-
+    if(createFinish)
+    finished.push_back(u);
 }
-int main()
-{
- vector< lli > graph[10];
 
-
-graph[1].push_back(2);
-graph[2].push_back(3);
-graph[3].push_back(1);
-
-graph[2].push_back(4);
-graph[4].push_back(5);
-
-get_SCC(graph,5);
-return 0;
+vector<int> SCC_Kosaraju() {
+    for(int i=1; i<=n; i++) {
+        if(!vis[i]) dfs(i, adj, true);
+    } 
+    vector<int> scc;
+    fill(vis.begin(), vis.end(), false);
+    for(int i=n-1; i>=0; i--) {
+        if(!vis[finished[i]]) {
+            scc.push_back(finished[i]);
+            dfs(finished[i], rev_adj, false);
+        }
+    }
+    return scc;
 }
